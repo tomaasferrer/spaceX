a0a55409b9a26430f9293a9d0a9729e372f8f004
Tomas <tomas.ferrer2802@gmail.com>
2023-04-26 11:37:36 -0300

    Three Commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import babel from "@rollup/plugin-babel";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    babel({
+      babelHelpers: "bundled",
+      presets: ["@babel/preset-react"],
+    }),
+  ],
+});
