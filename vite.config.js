ed1cc9810c5607da3d96b678bd67d13960cd54b6
Tomas <tomas.ferrer2802@gmail.com>
2023-04-26 12:59:55 -0300

    Test Bug

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "mime-fix",
-      transform(code, id) {
-        if (id.endsWith(".jsx")) {
-          return {
-            code,
-            map: null,
-            moduleSideEffects: "no-treeshake",
-            loader: "jsx",
-          };
-        }
-      },
-    },
-  ],
+  plugins: [react()],
+  base: "/spacexmission/",
 });
