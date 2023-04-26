41e0dbdb39bb8b5a4e94ff14859c37d133be75ab
Tomas <tomas.ferrer2802@gmail.com>
2023-04-26 11:55:04 -0300

    Five commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import babel from "@rollup/plugin-babel";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({
-      babelHelpers: "bundled",
-      presets: ["@babel/preset-react"],
-    }),
-  ],
+  plugins: [react()],
   server: {
     middleware: {
       handle(req, res, next) {
